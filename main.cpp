21d8f0b18a74fd5736f1702566126298c273aaae
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2018-12-12 16:59:40 +0900

    add hgemm

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
 {
     int method = 0;
     if (2 <= argc)
-        method = std::min(std::max(atoi(argv[1]), 0), 6);
+        method = std::min(std::max(atoi(argv[1]), 0), 16);
     uint64_t N = 512;
     if (3 <= argc)
         N = std::max(atoi(argv[2]), 8);
@@ -289,6 +289,90 @@ int main(int argc, char** argv)
                 vst1q_f32(C+(i0+1)*N+j0+4, vC13);
                 vst1q_f32(C+(i0+2)*N+j0+4, vC14);
                 vst1q_f32(C+(i0+3)*N+j0+4, vC15);
+            }
+            break;
+        case 10:
+            {
+                std::cout << "10" << std::endl;
+                float16_t *Ah = (float16_t*)A;
+                float16_t *Bh = (float16_t*)B;
+                float16_t *Ch = (float16_t*)C;
+                for (int i = 0;i < N;i++)
+                for (int j = 0;j < N;j++)
+                for (int k = 0;k < N;k++)
+                    Ch[i*N+j] += Ah[i*N+k] * Bh[k*N+j];
+            }
+            break;
+        case 16:
+            {
+                std::cout << "16" << std::endl;
+                float16_t *Ah = (float16_t*)A;
+                float16_t *Bh = (float16_t*)B;
+                float16_t *Ch = (float16_t*)C;
+                for (int i0 = 0;i0 < N;i0 += 8)
+                for (int j0 = 0;j0 < N;j0 += 8)
+                {
+                    float16x8_t vC0 = vdupq_n_f16(0);
+                    float16x8_t vC1 = vdupq_n_f16(0);
+                    float16x8_t vC2 = vdupq_n_f16(0);
+                    float16x8_t vC3 = vdupq_n_f16(0);
+                    float16x8_t vC4 = vdupq_n_f16(0);
+                    float16x8_t vC5 = vdupq_n_f16(0);
+                    float16x8_t vC6 = vdupq_n_f16(0);
+                    float16x8_t vC7 = vdupq_n_f16(0);
+                    float16x8_t vC8 = vdupq_n_f16(0);
+                    float16x8_t vC9 = vdupq_n_f16(0);
+                    float16x8_t vC10 = vdupq_n_f16(0);
+                    float16x8_t vC11 = vdupq_n_f16(0);
+                    float16x8_t vC12 = vdupq_n_f16(0);
+                    float16x8_t vC13 = vdupq_n_f16(0);
+                    float16x8_t vC14 = vdupq_n_f16(0);
+                    float16x8_t vC15 = vdupq_n_f16(0);
+                    for (int k = 0;k < N;k++)
+                    {
+                        float16x8_t vA0 = vdupq_n_f16(Ah[(i0+0)*N+k]);
+                        float16x8_t vA1 = vdupq_n_f16(Ah[(i0+1)*N+k]);
+                        float16x8_t vA2 = vdupq_n_f16(Ah[(i0+2)*N+k]);
+                        float16x8_t vA3 = vdupq_n_f16(Ah[(i0+3)*N+k]);
+                        float16x8_t vB0 = vld1q_f16(&Bh[(k+0)*N+j0]);
+                        float16x8_t vB1 = vld1q_f16(&Bh[(k+1)*N+j0]);
+                        float16x8_t vB2 = vld1q_f16(&Bh[(k+2)*N+j0]);
+                        float16x8_t vB3 = vld1q_f16(&Bh[(k+3)*N+j0]);
+
+                        vC0 = vfmaq_f16(vC0, vB0, vA0);
+                        vC1 = vfmaq_f16(vC1, vB0, vA1);
+                        vC2 = vfmaq_f16(vC2, vB0, vA2);
+                        vC3 = vfmaq_f16(vC3, vB0, vA3);
+                        vC4 = vfmaq_f16(vC4, vB1, vA0);
+                        vC5 = vfmaq_f16(vC5, vB1, vA1);
+                        vC6 = vfmaq_f16(vC6, vB1, vA2);
+                        vC7 = vfmaq_f16(vC7, vB1, vA3);
+                        vC8 = vfmaq_f16(vC8, vB2, vA0);
+                        vC9 = vfmaq_f16(vC9, vB2, vA1);
+                        vC10 = vfmaq_f16(vC10, vB2, vA2);
+                        vC11 = vfmaq_f16(vC11, vB2, vA3);
+                        vC12 = vfmaq_f16(vC12, vB3, vA0);
+                        vC13 = vfmaq_f16(vC13, vB3, vA1);
+                        vC14 = vfmaq_f16(vC14, vB3, vA2);
+                        vC15 = vfmaq_f16(vC15, vB3, vA3);
+                    }
+                    vst1q_f16(Ch+(i0+0)*N+j0+0, vC0);
+                    vst1q_f16(Ch+(i0+1)*N+j0+0, vC1);
+                    vst1q_f16(Ch+(i0+2)*N+j0+0, vC2);
+                    vst1q_f16(Ch+(i0+3)*N+j0+0, vC3);
+                    vst1q_f16(Ch+(i0+4)*N+j0+0, vC4);
+                    vst1q_f16(Ch+(i0+5)*N+j0+0, vC5);
+                    vst1q_f16(Ch+(i0+6)*N+j0+0, vC6);
+                    vst1q_f16(Ch+(i0+7)*N+j0+0, vC7);
+                    vst1q_f16(Ch+(i0+0)*N+j0+4, vC8);
+                    vst1q_f16(Ch+(i0+1)*N+j0+4, vC9);
+                    vst1q_f16(Ch+(i0+2)*N+j0+4, vC10);
+                    vst1q_f16(Ch+(i0+3)*N+j0+4, vC11);
+                    vst1q_f16(Ch+(i0+0)*N+j0+4, vC12);
+                    vst1q_f16(Ch+(i0+1)*N+j0+4, vC13);
+                    vst1q_f16(Ch+(i0+2)*N+j0+4, vC14);
+                    vst1q_f16(Ch+(i0+3)*N+j0+4, vC15);
+                }
             }
             break;
         }
